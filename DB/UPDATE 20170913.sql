2e384795afa814d3defa02478817053c49fdc121
klusanguinetti@gmail.com <klusanguinetti@gmail.com>
2017-09-13 22:55:26 -0300

    Mod

--- DB/UPDATE 20170913.sql
+++ DB/UPDATE 20170913.sql
@@ -1,7 +1,10 @@
-/*Modificaciones tabla jugador*/
+/*Modificaciones tabla Entrenador*/
 use BusDep
 go
 
-ALTER TABLE Entrenador ADD FotoRostro nvarchar(200);
+ALTER TABLE Entrenador ADD FotoRostro nvarchar(400);
 ALTER TABLE Entrenador ADD Perfil nvarchar(200);
+ALTER TABLE JUGADOR ALTER COLUMN FotoRostro varchar(400);
+ALTER TABLE JUGADOR ALTER COLUMN FotoCuertoEntero varchar(400);
+ALTER TABLE JUGADOR ALTER COLUMN VideoUrl varchar(400);
 go
