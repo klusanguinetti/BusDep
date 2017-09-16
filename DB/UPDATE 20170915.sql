de5321d4deccd9fde91625f40246a3fbaf1b675b
klusanguinetti@gmail.com <klusanguinetti@gmail.com>
2017-09-15 21:43:28 -0300

    mod db

---
+++ DB/UPDATE 20170915.sql
@@ -0,0 +1,33 @@
+use busdep
+go
+DECLARE @TipoUsuario_id numeric(10), @Descripcion varchar(200);
+
+DECLARE _cursor CURSOR FOR
+SELECT TipoUsuarioId, Descripcion FROM TipoUsuario;
+
+OPEN _cursor;
+FETCH NEXT FROM _cursor
+INTO @TipoUsuario_id, @Descripcion;
+
+-- Check @@FETCH_STATUS to see if there are any more rows to fetch.
+WHILE @@FETCH_STATUS = 0
+BEGIN
+
+   -- Concatenate and display the current values in the variables.
+   PRINT 'info: ' + convert(varchar(10),@TipoUsuario_id) + ' ' +  @Descripcion
+
+   
+   if @Descripcion = 'Entrenador'
+   begin
+		insert into Menu (TipoUsuarioId, Descripcion, Url, Icono, Orden, Estado)
+		values (@TipoUsuario_id, 'Antecedentes', '#!Coach/SportsHistory/List','fa fa-futbol-o', 2, 'A')
+   end
+  
+   -- This is executed as long as the previous fetch succeeds.
+   FETCH NEXT FROM _cursor
+   INTO @TipoUsuario_id, @Descripcion;
+END
+
+CLOSE _cursor;
+DEALLOCATE _cursor;
+go
